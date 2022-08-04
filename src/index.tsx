0a766c8650e29ffbc9b397b2fb36489464758bf6
Gavin Rehkemper <grehkemper@esri.com>
2022-08-04 13:15:20 -0500

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -16,15 +16,13 @@ import "@esri/calcite-components/dist/components/calcite-modal";
 setAssetPath("https://unpkg.com/@esri/calcite-components/dist/calcite/assets");
 
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const container = document.getElementById('root');
+if(container) {
+  const root = createRoot(container);
+  root.render(<React.StrictMode><App /></React.StrictMode>);
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+}
